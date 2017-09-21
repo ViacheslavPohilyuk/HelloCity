45143f29faca1b4a9d3873c2c89fef92838f2701
ViacheslavPohilyuk <vvpohil@gmail.com>
2017-09-21 16:46:03 +0300

    Support of internalized messages has been added

---
+++ src/main/java/CityTimeRecognizer.java
@@ -0,0 +1,109 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
+
+/**
+ * Created by mac on 02.09.17.
+ */
+public class CityTimeRecognizer {
+
+    private String cityName;
+
+    public CityTimeRecognizer(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void cityMessage() throws URISyntaxException {
+
+        System.out.println("Locale: " + Locale.getDefault());
+
+        URIBuilder builder = new URIBuilder();
+        builder.setScheme("http").setHost("maps.googleapis.com").setPath("/maps/api/geocode/json")
+                .setParameter("address", cityName)
+                .setParameter("sensor", "false");
+        String cityInfoJson = httpGetJsonByUrl(builder);
+
+        JSONObject location = new JSONObject(cityInfoJson)
+                .getJSONArray("results")
+                .getJSONObject(0)
+                .getJSONObject("geometry")
+                .getJSONObject("location");
+        Double latitude = location.getDouble("lat");
+        Double longitude = location.getDouble("lng");
+
+        //----------------------------------------------------------------------------------------------
+
+        builder = new URIBuilder();
+        builder.setScheme("https").setHost("maps.googleapis.com").setPath("/maps/api/timezone/json")
+                .setParameter("location", latitude + "," + longitude)
+                .setParameter("timestamp", "0");
+        String timeZoneJson = httpGetJsonByUrl(builder);
+
+        String cityTimeZoneId = new JSONObject(timeZoneJson)
+                .getString("timeZoneId");
+
+        //----------------------------------------------------------------------------------------------
+
+        LocalTime currentCityTime = LocalTime.now(ZoneId.of(cityTimeZoneId));
+        currentCityTime = currentCityTime.truncatedTo(MINUTES);
+
+        System.out.println(currentCityTime.toString());
+
+        double hourMinutes = currentCityTime.getHour() + currentCityTime.getMinute() / 60.0;
+
+        String greeting = "";
+        if (hourMinutes >= 6 && hourMinutes <= 9)
+            greeting = "morning";
+        else if (hourMinutes >= 9 && hourMinutes <= 19)
+            greeting = "afternoon";
+        else if (hourMinutes >= 19 && hourMinutes <= 23)
+            greeting = "evening";
+        else
+            greeting = "night";
+
+        Locale defaultLocale = Locale.getDefault();
+        Locale USLocale = new Locale("en", "US");
+
+        ResourceBundle bundle = ResourceBundle.getBundle("Greetings", USLocale);
+        String messageBundle = bundle.getString(greeting);
+
+        System.out.println(messageBundle + ", " + cityName + "!");
+    }
+
+    private String httpGetJsonByUrl(URIBuilder builder) throws URISyntaxException {
+        HttpClient client = HttpClientBuilder.create().build();
+        URI uri = builder.build();
+        HttpGet request = new HttpGet(uri);
+
+        StringBuilder result = new StringBuilder();
+        try {
+            HttpResponse response = client.execute(request);
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            result = new StringBuilder();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,21 @@
-import java.util.InputMismatchException;
+import java.net.URISyntaxException;
 import java.util.Scanner;
+
 
 /**
  * Created by mac on 01.09.17.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
         String cityName;
-        String zoneId;
+        String timeZoneId;
 
         try {
             // Name of a city from the command line
             cityName = args[0];
 
             // Id of time zone from command line
-            zoneId = args[1];
+            timeZoneId = args[1];
         }
 
         // Enter data after opening the program:
@@ -23,11 +24,12 @@ public class Main {
             Scanner scCity = new Scanner(System.in);
             cityName = scCity.next();
 
-            System.out.println("Enter id of the time zone (s - skip | e - exit):");
-            Scanner scZoneId = new Scanner(System.in);
-            zoneId = scZoneId.next();
+            // System.out.println("Enter id of the time zone (s - skip | e - exit):");
+            // Scanner scZoneId = new Scanner(System.in);
+            // timeZoneId = scZoneId.next();
         }
 
-
+        CityTimeRecognizer cityTimeRecognizer = new CityTimeRecognizer(cityName);
+        cityTimeRecognizer.cityMessage();
     }
 }
